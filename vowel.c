8bcc1435c05c9a0f440ee7f8ea540fd3f79608dd
gde127 <35266992+gde127@users.noreply.github.com>
2018-01-10 07:31:30 -0800

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if(ch=='a'||ch=='A'||ch='e'||ch==E||ch==i||ch==o||ch==O||ch==u||ch==U)
+printf("vowel is present");
+else
+printf("vowel is not present");
+}
