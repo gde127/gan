f901c8481144ff3b8793fae29ecb1c9d8b29a5f2
gde127 <35266992+gde127@users.noreply.github.com>
2018-03-02 16:47:05 +0530

    Create eveni.c

---
+++ eveni.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+ int g;
+ printf("enter the number");
+ scanf("%d",&g);
+ g=g-1;
+ if(g%2==0)
+ printf("even number",g);
+ else
+ printf(" ");
+ return(0);
+}
