bbba160e83055a4b0b164cd7dd47733664415416
gde127 <35266992+gde127@users.noreply.github.com>
2018-03-02 16:02:23 +0530

    Create word.c

---
+++ word.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 1:
+        printf("/n one");
+        break;
+        case 2:
+        printf("/n two");
+        break;
+        case 3:
+        printf("/nthree");
+        break;
+        case 4:
+        printf("/nfour");
+        break;
+        case 5:
+        printf("/nfive");
+        break;
+        case 6:
+        printf("/nsix");
+        break;
+        case 7:
+        printf("/nseven");
+        break;
+        case 8:
+        printf("/neight");
+        break;
+        case 9:
+        printf("/nnine");
+        break;
+        case 10:
+        printf("/nten");
+        break;
+        return(0);
+    }
+}
