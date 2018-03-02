098180a38d781ede290af9c3c1355ac8188ddd67
gde127 <35266992+gde127@users.noreply.github.com>
2018-03-02 16:24:40 +0530

    Create prop.c

---
+++ prop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+ int m,n,o;
+ printf("enter the number");
+ scanf("%d%d",&m,&n);
+ o=m*n;
+ if(o%2==0)
+ printf("even number");
+ else
+ printf("odd number");
+
+    return 0;
+}
