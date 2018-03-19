97848e11309d5d6e591f4d572b0697525e401f6e
gde127 <35266992+gde127@users.noreply.github.com>
2018-03-19 16:27:56 +0530

    Create thirteen.c

---
+++ thirteen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n%13==0)
+    printf("multiples of 13");
+    else
+    printf("not a multiples of 13");
+
+    return 0;
+}
